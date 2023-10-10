eaf34a69add58ffba96e24e791589c3c52311cc7
SirotaSia <an.sirota1608@gmail.com>
2023-10-10 19:39:27 +0300

    Выполнила первую домашнюю работу.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Skypro_AQA_HW
 Решение домашних заданий на курсе Автоматизация тестирования на Python от Skypro
 ## Домашняя работа №1
-sp_aqa_hw1.py
+Домашняя работа лежит в папке:lesson_1
+Задание описано в: https://skyengpublic.notion.site/Python-39cd1b8d1e764b03b08aa0d182124cd0
---
+++ lesson_1/lesson_1_task_7.py
@@ -0,0 +1,41 @@
+def print_to_term_0():
+    print(0, end = "")
+
+def print_to_term_1():
+    print(1, end = "")
+
+def print_to_term_2():
+    print(2, end = "")
+
+def print_to_term_3():
+    print(3, end = "")
+
+def print_to_term_4():
+    print(4, end = "")
+
+def print_to_term_5():
+    print(5, end = "")
+
+def print_to_term_6():
+    print(6, end = "")
+
+def print_to_term_7():
+    print(7, end = "")
+
+def print_to_term_8():
+    print(8, end = "")
+
+def print_to_term_9():
+    print(9, end = "")
+
+print_to_term_8()
+print_to_term_8()
+print_to_term_0()
+print_to_term_0()
+print_to_term_5()
+print_to_term_5()
+print_to_term_5()
+print_to_term_3()
+print_to_term_5()
+print_to_term_3()
+print_to_term_5()
